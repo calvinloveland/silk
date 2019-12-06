88d86f2a1cbbe1a4e7e2c37f7a58b503edb90052
link270 <leafytwig@hotmail.com>
2019-12-05 18:26:43 -0700

    Got routing working

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,22 @@ router.get("/", (req, res) => {
         title: "Silk"
     })
 });
+
+router.get("/submit", (req, res) => {
+    // renders the submit.ejs page
+    res.render("submit", {
+        // This gets rendered as the browsers title
+        // it is passed into the ejs page as the variable 'title'
+        title: "Silk | Submit"
+    })
+});
+
+router.get("/grades", (req, res) => {
+    // renders the grades.ejs page
+    res.render("grades", {
+        // This gets rendered as the browsers title
+        // it is passed into the ejs page as the variable 'title'
+        title: "Silk | Grades"
+    })
+});
 module.exports = router;
